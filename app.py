b8dea8a83042ea6e44ee3fce453aedbdd1eba837
willwaltrip <willwaltrip@gmail.com>
2021-01-17 10:12:35 -0500

    Updated where db was placed. Updated uswgi.ini

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-from resources import UserRegister
-from resources import Item, Items
+from resources.user import UserRegister
+from resources.item import Item, Items
 from resources.store import Store, StoreList
 from db import db
 
@@ -14,10 +14,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "pasta"
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
